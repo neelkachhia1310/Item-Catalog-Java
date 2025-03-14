1b922925fab59113abc52cc80c78aa018bdd931f
Neel Kachhia <neel.kachhia13@gmail.com>
2025-03-14 19:56:45 -0400

    item catalog

---
+++ src/main/java/com/example/demo/Item.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Item {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Item(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Item() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", name=" + name + "]";
+	}
+
+}
